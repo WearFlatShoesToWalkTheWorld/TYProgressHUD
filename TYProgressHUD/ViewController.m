771cb1e273648fba488f788fa080acbcb92c9b68
WearFlatShoesToWalkTheWorld <15998581435@sina.cn>
2018-09-28 18:22:29 +0800

    push

--- TYProgressHUD/ViewController.m
+++ TYProgressHUD/ViewController.m
@@ -7,17 +7,47 @@
 //
 
 #import "ViewController.h"
-
+#import "TYProgressHUD.h"
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
-
+{
+    NSInteger index;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+ 
 }
 
-
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    
+//    if (index == 0){
+//        [TYProgressHUD showSuccess:@"成功~~~成功~~~成功~~~" zoom: YES shadow:YES];
+//    }else if (index == 1){
+//        [TYProgressHUD showFailure:@"失败~~~" zoom:YES shadow:YES];
+//    }else if (index == 2){
+//        [TYProgressHUD showWarning:@"警告" zoom:YES shadow:YES];
+//    }else if (index == 3){
+//
+//    }else if (index == 4){
+//        
+//    }
+//    index ++;
+//    [TYProgressHUD  showLoading:@""];
+    
+    [TYProgressHUD showText:@"已无更多博主信息" Location:TYProgressHUDViewTop zoom:true shadow:true];
+//    [TYProgressHUD showText:@"已无更多博主信息" Location:TYProgressHUDViewCenter zoom:true shadow:true];
+//    [TYProgressHUD showWarning:@"已无更多博主信息" zoom:YES shadow:YES];
+//    [TYProgressHUD showSuccess:@"已无更多博主信息" zoom:YES shadow:YES];
+    [TYProgressHUD showFailure:@"已无更多博主信息" zoom:YES shadow:YES];
+//    [TYProgressHUD showLoading:@"" inView:self.view];
+//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+//        [TYProgressHUD hideInKeyWindow];
+//    });
+    
+}
 @end
